e85f526e2a28bd24e99f0733724c9266fce1e21a
Jakob Hansen <jakob.hansen@hotmail.no>
2021-11-21 01:29:03 +0100

    Hypothesis test

--- src/tests.py
+++ src/tests.py
@@ -1,8 +1,20 @@
 from radix_sequential import radixsort
 
+import hypothesis as hyp
+import hypothesis.strategies as st
+
+from utils import successfully_sorted
+
+@hyp.given(st.lists(st.integers()))
+@hyp.settings(max_examples=1000)
+def test_sequential_radix(array):
+    radix_sorted = radixsort(array)
+
+    assert(successfully_sorted(array, radix_sorted))
+
 
 def usage_test():
-    array = [9,5,3,7,4,1,4, 21, 97, 105]
+    array = [9,5,3,7,4,1,4, 21, 97, 105, -20, -10]
     print(radixsort(array))
 
-usage_test()
+test_sequential_radix()
